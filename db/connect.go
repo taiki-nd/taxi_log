cffb4e586d246b367c735e07cdfe1f7c3a2a85c4
taiki-nd <kii.develop.0517@gmail.com>
2022-09-16 20:00:57 +0900

    db接続

---
+++ db/connect.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/taiki-nd/taxi_log/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+/**
+ * ConnectToDb
+ * db接続の設定
+ */
+func ConnectToDb() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",
+		config.Config.User, config.Config.Password,
+		config.Config.Host, config.Config.Port,
+		config.Config.Name)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("db connection error: %v", err)
+	}
+
+	log.Println("db connection success")
+
+	DB = db
+
+}
