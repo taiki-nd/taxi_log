ca160b19005f6e5203a16fb2e327b8301ce4ed2c
taiki-nd <kii.develop.0517@gmail.com>
2022-09-18 23:50:38 +0900

    user createメソッド

--- api/v1/controller/usersController.go
+++ api/v1/controller/usersController.go
@@ -1,7 +1,78 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/taiki-nd/taxi_log/db"
+	"github.com/taiki-nd/taxi_log/model"
+	"github.com/taiki-nd/taxi_log/service"
+)
 
 func UsersIndex(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
+}
+
+/**
+ * UsersCreate
+ * userの新規登録処理
+ * @params c *fiber.Ctx
+ * @returns error error
+ */
+func UsersCreate(c *fiber.Ctx) error {
+	// 変数確認
+	var user *model.User
+
+	// user認証
+
+	// リクエストボディーのパース
+	err := c.BodyParser(&user)
+	if err != nil {
+		log.Printf("body parse error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "body_parse_error",
+				"message": fmt.Sprintf("body parse error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// バリデーション
+	_, errs := service.UserValidation(user)
+	if len(errs) != 0 {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    errs,
+				"message": fmt.Sprintf("validation error: %v", errs),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// レコード作成
+	err = db.DB.Create(&user).Error
+	if err != nil {
+		log.Printf("db error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "db_error",
+				"message": fmt.Sprintf("db error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"info": fiber.Map{
+			"status":  true,
+			"code":    "create_user_success",
+			"message": "",
+		},
+		"data": user,
+	})
 }
