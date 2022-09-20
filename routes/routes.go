c319dc92cf191541aa61397cd1aaace45959d0dd
taiki-nd <kii.develop.0517@gmail.com>
2022-09-20 23:21:35 +0900

    ルーティング

--- routes/routes.go
+++ routes/routes.go
@@ -6,9 +6,18 @@ import (
 )
 
 func Routes(app *fiber.App) {
+
+	// user
 	app.Get("/api/v1/users", controller.UsersIndex)
 	app.Get("/api/v1/users/:id", controller.UsersShow)
 	app.Post("/api/v1/users", controller.UsersCreate)
 	app.Put("/api/v1/users/:id", controller.UsersUpdate)
 	app.Delete("/api/v1/users/:id", controller.UsersDelete)
+
+	// record
+	app.Get("/api/v1/records", controller.RecordsIndex)
+	app.Get("/api/v1/records/:id", controller.RecordsShow)
+	app.Post("/api/v1/records", controller.RecordsCreate)
+	app.Put("/api/v1/records/:id", controller.RecordsUpdate)
+	app.Delete("/api/v1/records/:id", controller.RecordsDelete)
 }
