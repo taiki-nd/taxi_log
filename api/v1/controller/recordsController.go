1c47eedf28cf427af6a814f4618ed7060f594599
taiki-nd <kii.develop.0517@gmail.com>
2022-09-21 22:33:31 +0900

    recordsのcrud処理

---
+++ api/v1/controller/recordsController.go
@@ -0,0 +1,401 @@
+package controller
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/taiki-nd/taxi_log/db"
+	"github.com/taiki-nd/taxi_log/model"
+	"github.com/taiki-nd/taxi_log/service"
+)
+
+/**
+ * RecordsIndex
+ * recordの一覧取得
+ * @params c *fiber.Ctx
+ * @returns error error
+ */
+func RecordsIndex(c *fiber.Ctx) error {
+	// user認証
+	statuses, errs, err := service.UserAuth(c)
+	if err != nil {
+		log.Printf("user auth error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_auth_error",
+				"message": fmt.Sprintf("user auth error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+	if len(errs) != 0 {
+		log.Println(errs)
+	}
+	// signin確認
+	if !statuses[0] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_not_signin",
+				"message": "user not signin",
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// recordの検索
+	records, err := service.SearchRecord(c, statuses[1])
+	if err != nil {
+		log.Printf("db error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "db_error",
+				"message": fmt.Sprintf("db error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"info": fiber.Map{
+			"status":  true,
+			"code":    "index_record_success",
+			"message": "",
+		},
+		"data": records,
+	})
+}
+
+/**
+ * RecordsShow
+ * recordの一覧取得
+ * @params c *fiber.Ctx
+ * @returns error error
+ */
+func RecordsShow(c *fiber.Ctx) error {
+	// user認証
+	statuses, errs, err := service.UserAuth(c)
+	if err != nil {
+		log.Printf("user auth error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_auth_error",
+				"message": fmt.Sprintf("user auth error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+	if len(errs) != 0 {
+		log.Println(errs)
+	}
+	// signin確認
+	if !statuses[0] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_not_signin",
+				"message": "user not signin",
+			},
+			"data": fiber.Map{},
+		})
+	}
+	// user合致確認
+	if !statuses[2] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_not_match",
+				"message": "user not match",
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// レコードの取得
+	record, err := service.GetRecord(c)
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "db_error",
+				"message": fmt.Sprintf("db error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"info": fiber.Map{
+			"status":  true,
+			"code":    "show_record_success",
+			"message": "",
+		},
+		"data": record,
+	})
+}
+
+/**
+ * RecordsCreate
+ * recordの新規登録処理
+ * @params c *fiber.Ctx
+ * @returns error error
+ */
+func RecordsCreate(c *fiber.Ctx) error {
+	// 変数確認
+	var record *model.Record
+
+	// user認証
+	statuses, errs, err := service.UserAuth(c)
+	if err != nil {
+		log.Printf("user auth error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_auth_error",
+				"message": fmt.Sprintf("user auth error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+	if len(errs) != 0 {
+		log.Println(errs)
+	}
+	// signin確認
+	if !statuses[0] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_not_signin",
+				"message": "user not signin",
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// リクエストボディーのパース
+	err = c.BodyParser(&record)
+	if err != nil {
+		log.Printf("body parse error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "body_parse_error",
+				"message": fmt.Sprintf("body parse error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// バリデーション
+	_, errs = service.RecordValidation(record)
+	if len(errs) != 0 {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    errs,
+				"message": fmt.Sprintf("validation error: %v", errs),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// レコード作成
+	err = db.DB.Create(&record).Error
+	if err != nil {
+		log.Printf("db error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "db_error",
+				"message": fmt.Sprintf("db error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"info": fiber.Map{
+			"status":  true,
+			"code":    "create_record_success",
+			"message": "",
+		},
+		"data": record,
+	})
+}
+
+/**
+ * RecordsUpdate
+ * record情報の更新処理
+ * @params c *fiber.Ctx
+ */
+func RecordsUpdate(c *fiber.Ctx) error {
+	// user認証
+	statuses, errs, err := service.UserAuth(c)
+	if err != nil {
+		log.Printf("user auth error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_auth_error",
+				"message": fmt.Sprintf("user auth error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+	if len(errs) != 0 {
+		log.Println(errs)
+	}
+	// signin確認
+	if !statuses[0] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "record_not_signin",
+				"message": "record not signin",
+			},
+			"data": fiber.Map{},
+		})
+	}
+	// user合致確認
+	if !statuses[2] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_not_match",
+				"message": "user not match",
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// recordレコードの取得
+	record, err := service.GetRecord(c)
+	if err != nil {
+		log.Printf("db error: %v", err)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"code":   "db_error",
+			"data":   fiber.Map{},
+		})
+	}
+
+	// リクエストボディのパース
+	err = c.BodyParser(record)
+	if err != nil {
+		log.Printf("body parse error: %v", err)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"code":   "body_parse_error",
+			"data":   fiber.Map{},
+		})
+	}
+
+	// バリデーション
+	_, errs = service.RecordValidation(record)
+	if len(errs) != 0 {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    errs,
+				"message": fmt.Sprintf("validation error: %v", errs),
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// record情報の更新
+	err = db.DB.Model(&record).Updates(record).Error
+	if err != nil {
+		log.Printf("db error: %v", err)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"code":   "db_error",
+			"data":   fiber.Map{},
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"status": true,
+		"code":   "update_record_success",
+		"data":   record,
+	})
+}
+
+/**
+ * RecordsDelete
+ * record情報の削除
+ * @params c *fiber.Ctx
+ * @returns error
+ */
+func RecordsDelete(c *fiber.Ctx) error {
+	// user認証
+	statuses, errs, err := service.UserAuth(c)
+	if err != nil {
+		log.Printf("user auth error: %v", err)
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_auth_error",
+				"message": fmt.Sprintf("user auth error: %v", err),
+			},
+			"data": fiber.Map{},
+		})
+	}
+	if len(errs) != 0 {
+		log.Println(errs)
+	}
+	// signin確認
+	if !statuses[0] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_not_signin",
+				"message": "user not signin",
+			},
+			"data": fiber.Map{},
+		})
+	}
+	// user合致確認
+	if !statuses[2] {
+		return c.JSON(fiber.Map{
+			"info": fiber.Map{
+				"status":  false,
+				"code":    "user_not_match",
+				"message": "user not match",
+			},
+			"data": fiber.Map{},
+		})
+	}
+
+	// recordレコードの取得
+	record, err := service.GetRecord(c)
+	if err != nil {
+		log.Printf("db error: %v", err)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"code":   "db_error",
+			"data":   fiber.Map{},
+		})
+	}
+
+	// record情報の削除
+	errRecord := db.DB.Delete(record).Error
+	if errRecord != nil {
+		log.Printf("db error: %v", err)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"code":   "db_error",
+			"data":   fiber.Map{},
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"status": true,
+		"code":   "delete_record_success",
+		"data":   fiber.Map{},
+	})
+}
---
+++ service/recordLogic.go
@@ -0,0 +1,114 @@
+package service
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/taiki-nd/taxi_log/db"
+	"github.com/taiki-nd/taxi_log/model"
+)
+
+/**
+ * SearchRecord
+ * recordsの検索
+ * @params c *fiber.Ctx
+ * @returns records []*models.Record
+ */
+func SearchRecord(c *fiber.Ctx, adminStatus bool) ([]*model.Record, error) {
+	var records []*model.Record
+
+	// paramsの確認
+	data := c.Query("data")
+	day_of_week := c.Query("day_of_week")
+	style_flg := c.Query("style_flg")
+	occupancy_rate := c.Query("occupancy_rate")
+	daily_sales := c.Query("daily_sales")
+
+	// クエリの作成
+	recordSearch := db.DB.Where("")
+	if len(data) != 0 {
+		recordSearch.Where("data = ?", data)
+	}
+	if len(day_of_week) != 0 {
+		recordSearch.Where("day_of_week = ?", day_of_week)
+	}
+	if len(style_flg) != 0 {
+		recordSearch.Where("style_flg = ?", style_flg)
+	}
+	if len(occupancy_rate) != 0 {
+		recordSearch.Where("occupancy_rate >= ?", occupancy_rate)
+	}
+	if len(daily_sales) != 0 {
+		recordSearch.Where("daily_sales >= ?", daily_sales)
+	}
+	// admin権限の確認
+	user, err := GetUserFromUuid(c)
+	if err != nil {
+		return nil, err
+	}
+	if !user.IsAdmin {
+		recordSearch.Where("user_id = ?", user.Id)
+	}
+
+	// recordsレコードの取得
+	err = recordSearch.Find(&records).Error
+	if err != nil {
+		return nil, err
+	}
+	return records, nil
+}
+
+/**
+ * RecordValidation
+ * recordのバリデーション機能
+ * @params record *model.Record
+ * @returns bool
+ * @returns []string
+ */
+func RecordValidation(record *model.Record) (bool, []string) {
+	var errs []string
+
+	// day_of_week
+	if len(record.DayOfWeek) == 0 {
+		log.Println("day_of_week null error")
+		errs = append(errs, "day_of_week_null_error")
+	}
+	// style_flg
+	if len(record.StyleFlg) == 0 {
+		log.Println("style_flg null error")
+		errs = append(errs, "style_flg_null_error")
+	}
+	if !(record.StyleFlg == "every_other_day" || record.StyleFlg == "day" || record.StyleFlg == "night" || record.StyleFlg == "other") {
+		log.Println("specified word error(style_flg)")
+		errs = append(errs, "specified_word_error(style_flg)")
+	}
+
+	// errの出力
+	if len(errs) != 0 {
+		return false, errs
+	}
+
+	return true, nil
+}
+
+/**
+ * GetRecord
+ * record情報の取得
+ * @params c *fiber.Ctx
+ * @returns record *model.Record
+ */
+func GetRecord(c *fiber.Ctx) (*model.Record, error) {
+	// 変数確認
+	record_id := c.Params("id")
+	var record *model.Record
+
+	// レコードの取得
+	err := db.DB.Where("id = ?", record_id).First(&record).Error
+	if err != nil {
+		log.Printf("db error: %v", err)
+		return nil, fmt.Errorf("db_error")
+	}
+
+	return record, nil
+}
