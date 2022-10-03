908e85f664eaafb465b4bfba9b4a81b01310eaee
taiki-nd <kii.develop.0517@gmail.com>
2022-10-03 23:33:03 +0900

    record詳細表示のfollow認証

--- service/followLogic.go
+++ service/followLogic.go
@@ -19,6 +19,7 @@ type Follow struct {
  * follower確認
  * @params c *fiber.Ctx
  * @returns bool
+ * @returns error
  */
 func IsFollower(c *fiber.Ctx) (bool, error) {
 	// 変数確認
@@ -27,7 +28,29 @@ func IsFollower(c *fiber.Ctx) (bool, error) {
 	var follow model.UserFollowing
 
 	// follow関係の確認
-	err := db.DB.Table("user_followings").Where("user_id = ?", signin_user.Id).Where("following_id = ?", id).First(&follow).Error
+	err := db.DB.Table("user_followings").Where("user_id = ?", signin_user.Id).Where("following_id = ?", id).Where("permission = ?", true).First(&follow).Error
+	if err != nil {
+		return false, fmt.Errorf("follow_relationship_error: %v", err)
+	} else {
+		return true, nil
+	}
+}
+
+/**
+ * IsFollowerForRecord
+ * follower確認
+ * @params c *fiber.Ctx
+ * @returns bool
+ * @returns error
+ */
+func IsFollowerForRecord(c *fiber.Ctx, record *model.Record) (bool, error) {
+	// 変数確認
+	signin_user, _ := GetUserFromUuid(c)
+	recorded_user_id := record.UserId
+	var follow model.UserFollowing
+
+	// follow関係の確認
+	err := db.DB.Table("user_followings").Where("user_id = ?", signin_user.Id).Where("following_id = ?", recorded_user_id).Where("permission = ?", true).First(&follow).Error
 	if err != nil {
 		return false, fmt.Errorf("follow_relationship_error: %v", err)
 	} else {
