81a8d576335eab03bb74f1b9c565044713e177e2
taiki-nd <kii.develop.0517@gmail.com>
2022-12-06 23:53:32 +0900

    analysisページ用のコントローラーの作成

--- controller/api/v1/AnalysisController.go
+++ controller/api/v1/AnalysisController.go
@@ -89,3 +89,23 @@ func GetRecords(c *fiber.Ctx) error {
 
 	return service.SuccessResponse(c, []string{"success_get_records"}, records, nil)
 }
+
+/**
+ * AnalysisPage
+ */
+func AnalysisPage(c *fiber.Ctx) error {
+	// 曜日別平均の解析
+	// 曜日別平均売上
+	// 曜日別平均実車率
+	// 曜日別平均乗車回数
+	// 曜日別平均走行距離
+	// 乗車方式別平均売上
+	data, err := service.GetAllAnalysisData(c)
+	if err != nil {
+		return service.ErrorResponse(c, []string{constants.DB_ERR}, fmt.Sprintf("db error: %v", err))
+	}
+
+	return c.JSON(fiber.Map{
+		"data": data,
+	})
+}
