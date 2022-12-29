c4f6cfea863a4b9a49fae702b448e0df46a3cef2
taiki-nd <kii.develop.0517@gmail.com>
2022-12-30 00:24:29 +0900

    indexの追加

--- service/RankingLogic.go
+++ service/RankingLogic.go
@@ -46,49 +46,49 @@ func GetRankingData(c *fiber.Ctx) (interface{}, error) {
 	var monthly_ranking_night_records []*model.Record
 
 	// 昨日のランキング（隔日勤務）
-	err := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "every_other_day").Order("daily_sales DESC").Find(&daily_ranking_every_other_day_records).Error
+	err := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "every_other_day").Order("daily_sales DESC").Find(&daily_ranking_every_other_day_records).Error
 	if err != nil {
 		return nil, err
 	}
 	// 昨日のランキング（日勤）
-	err2 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "day").Order("daily_sales DESC").Find(&daily_ranking_day_records).Error
+	err2 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "day").Order("daily_sales DESC").Find(&daily_ranking_day_records).Error
 	if err2 != nil {
 		return nil, err
 	}
 	// 昨日のランキング（夜勤）
-	err3 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "night").Order("daily_sales DESC").Find(&daily_ranking_night_records).Error
+	err3 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "night").Order("daily_sales DESC").Find(&daily_ranking_night_records).Error
 	if err3 != nil {
 		return nil, err
 	}
 
 	// 7日間のランキング（隔日勤務）
-	err4 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "every_other_day").Order("daily_sales DESC").Find(&weekly_ranking_every_other_day_records).Error
+	err4 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "every_other_day").Order("daily_sales DESC").Find(&weekly_ranking_every_other_day_records).Error
 	if err4 != nil {
 		return nil, err
 	}
 	// 7日間のランキング（日勤）
-	err5 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "day").Order("daily_sales DESC").Find(&weekly_ranking_day_records).Error
+	err5 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "day").Order("daily_sales DESC").Find(&weekly_ranking_day_records).Error
 	if err5 != nil {
 		return nil, err
 	}
 	// 7日間のランキング（夜勤）
-	err6 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "night").Order("daily_sales DESC").Find(&weekly_ranking_night_records).Error
+	err6 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "night").Order("daily_sales DESC").Find(&weekly_ranking_night_records).Error
 	if err6 != nil {
 		return nil, err
 	}
 
 	// 31日間のランキング（隔日勤務）
-	err7 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "every_other_day").Order("daily_sales DESC").Find(&monthly_ranking_every_other_day_records).Error
+	err7 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "every_other_day").Order("daily_sales DESC").Find(&monthly_ranking_every_other_day_records).Error
 	if err7 != nil {
 		return nil, err
 	}
 	// 31日間のランキング（日勤）
-	err8 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "day").Order("daily_sales DESC").Find(&monthly_ranking_day_records).Error
+	err8 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "day").Order("daily_sales DESC").Find(&monthly_ranking_day_records).Error
 	if err8 != nil {
 		return nil, err
 	}
 	// 31日間のランキング（夜勤）
-	err9 := rankingDataQuery.Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "night").Order("daily_sales DESC").Find(&monthly_ranking_night_records).Error
+	err9 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "night").Order("daily_sales DESC").Find(&monthly_ranking_night_records).Error
 	if err9 != nil {
 		return nil, err
 	}
