046e3c9bfb6280b18257e83410fb9737eee79a3d
taiki-nd <kii.develop.0517@gmail.com>
2023-01-18 00:07:26 +0900

    corsの設定

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,12 +3,13 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/taiki-nd/taxi_log/config"
 )
 
 func Cors(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 		AllowHeaders: "Content-Type, Content-Length, Authorization",
-		AllowOrigins: "*",
+		AllowOrigins: config.Config.Url,
 	}))
 }
