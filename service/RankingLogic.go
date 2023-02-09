b31ccaec5220db440e5dc1c385041beda4f40c0e
taiki-nd <kii.develop.0517@gmail.com>
2023-02-09 21:30:36 +0900

    go-funk導入

--- service/RankingLogic.go
+++ service/RankingLogic.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -81,105 +80,40 @@ func GetRankingData(c *fiber.Ctx) (interface{}, error) {
 	//
 
 	// 1カ月分の分類
-	monthly_every_other_day_records, monthly_day_records, monthly_ranking_night_records := RecordsClassification(records_for_month)
+	monthly_every_other_day_records, monthly_day_records, monthly_night_records := RecordsClassification(records_for_month)
 
 	// 1週間分の分類
-	weekly_every_other_day_records, weekly_day_records, weekly_ranking_night_records := RecordsClassification(records_for_week)
+	weekly_every_other_day_records, weekly_day_records, weekly_night_records := RecordsClassification(records_for_week)
 
 	// 1日分の分類
-	daily_every_other_day_records, daily_day_records, daily_ranking_night_records := RecordsClassification(records_for_day)
-
-	fmt.Printf("monthly_every_other_day_records %v \n", monthly_every_other_day_records)
-	fmt.Printf("monthly_day_records%v \n", monthly_day_records)
-	fmt.Printf("monthly_ranking_night_records%v \n", monthly_ranking_night_records)
-
-	fmt.Printf("weekly_every_other_day_records%v \n", weekly_every_other_day_records)
-	fmt.Printf("weekly_day_records%v \n", weekly_day_records)
-	fmt.Printf("weekly_ranking_night_records%v \n", weekly_ranking_night_records)
-
-	fmt.Printf("daily_every_other_day_records%v \n", daily_every_other_day_records)
-	fmt.Printf("daily_day_records%v \n", daily_day_records)
-	fmt.Printf("daily_ranking_night_records%v \n", daily_ranking_night_records)
+	daily_every_other_day_records, daily_day_records, daily_night_records := RecordsClassification(records_for_day)
 
 	//
 	// ランキング情報の抽出
 	//
 
-	/*
-		// 変数
-		var daily_ranking_every_other_day_records []*model.Record
-		var daily_ranking_day_records []*model.Record
-		var daily_ranking_night_records []*model.Record
+	var daily_ranking_every_other_day_records []Record
+	var daily_ranking_day_records []Record
+	var daily_ranking_night_records []Record
 
-		var weekly_ranking_every_other_day_records []*model.Record
-		var weekly_ranking_day_records []*model.Record
-		var weekly_ranking_night_records []*model.Record
+	var weekly_ranking_every_other_day_records []Record
+	var weekly_ranking_day_records []Record
+	var weekly_ranking_night_records []Record
 
-		var monthly_ranking_every_other_day_records []*model.Record
-		var monthly_ranking_day_records []*model.Record
-		var monthly_ranking_night_records []*model.Record
-
-		// 昨日のランキング（隔日勤務）
-		err := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "every_other_day").Order("daily_sales DESC").Find(&daily_ranking_every_other_day_records).Error
-		if err != nil {
-			return nil, err
-		}
-		// 昨日のランキング（日勤）
-		err2 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "day").Order("daily_sales DESC").Find(&daily_ranking_day_records).Error
-		if err2 != nil {
-			return nil, err
-		}
-		// 昨日のランキング（夜勤）
-		err3 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", yesterday_start, today, "night").Order("daily_sales DESC").Find(&daily_ranking_night_records).Error
-		if err3 != nil {
-			return nil, err
-		}
-
-		// 7日間のランキング（隔日勤務）
-		err4 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "every_other_day").Order("daily_sales DESC").Find(&weekly_ranking_every_other_day_records).Error
-		if err4 != nil {
-			return nil, err
-		}
-		// 7日間のランキング（日勤）
-		err5 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "day").Order("daily_sales DESC").Find(&weekly_ranking_day_records).Error
-		if err5 != nil {
-			return nil, err
-		}
-		// 7日間のランキング（夜勤）
-		err6 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", weekly_start, today, "night").Order("daily_sales DESC").Find(&weekly_ranking_night_records).Error
-		if err6 != nil {
-			return nil, err
-		}
-
-		// 31日間のランキング（隔日勤務）
-		err7 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "every_other_day").Order("daily_sales DESC").Find(&monthly_ranking_every_other_day_records).Error
-		if err7 != nil {
-			return nil, err
-		}
-		// 31日間のランキング（日勤）
-		err8 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "day").Order("daily_sales DESC").Find(&monthly_ranking_day_records).Error
-		if err8 != nil {
-			return nil, err
-		}
-		// 31日間のランキング（夜勤）
-		err9 := rankingDataQuery.Debug().Limit(constants.RANKING_LIMIT).Where("date >= ? && date <= ? && style_flg = ?", monthly_start, today, "night").Order("daily_sales DESC").Find(&monthly_ranking_night_records).Error
-		if err9 != nil {
-			return nil, err
-		}
-	*/
+	var monthly_ranking_every_other_day_records []Record
+	var monthly_ranking_day_records []Record
+	var monthly_ranking_night_records []Record
 
 	ranking_data := map[string]interface{}{
-		/*
-			"daily_ranking_every_other_day_records":   daily_ranking_every_other_day_records,
-			"daily_ranking_day_records":               daily_ranking_day_records,
-			"daily_ranking_night_records":             daily_ranking_night_records,
-			"weekly_ranking_every_other_day_records":  weekly_ranking_every_other_day_records,
-			"weekly_ranking_day_records":              weekly_ranking_day_records,
-			"weekly_ranking_night_records":            weekly_ranking_night_records,
-			"monthly_ranking_every_other_day_records": monthly_ranking_every_other_day_records,
-			"monthly_ranking_day_records":             monthly_ranking_day_records,
-			"monthly_ranking_night_records":           monthly_ranking_night_records,
-		*/
+		"daily_ranking_every_other_day_records":   daily_ranking_every_other_day_records,
+		"daily_ranking_day_records":               daily_ranking_day_records,
+		"daily_ranking_night_records":             daily_ranking_night_records,
+		"weekly_ranking_every_other_day_records":  weekly_ranking_every_other_day_records,
+		"weekly_ranking_day_records":              weekly_ranking_day_records,
+		"weekly_ranking_night_records":            weekly_ranking_night_records,
+		"monthly_ranking_every_other_day_records": monthly_ranking_every_other_day_records,
+		"monthly_ranking_day_records":             monthly_ranking_day_records,
+		"monthly_ranking_night_records":           monthly_ranking_night_records,
 	}
 
 	return ranking_data, nil
